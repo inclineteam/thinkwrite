fac96104fa30826d763212925c6691cab4f81675
Pam Patacsil <90546802+yskooo@users.noreply.github.com>
2023-06-07 15:22:15 +0800

    chore: update deprecated

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,16 +6,13 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import GitHubProvider from 'next-auth/providers/github';
 import prisma from '../../../lib/prisma';
 
-const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
-export default authHandler;
+import GithubProvider from "next-auth/providers/github"
 
-const options = {
+export default NextAuth({
   providers: [
-    GitHubProvider({
+    GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
-  adapter: PrismaAdapter(prisma),
-  secret: process.env.SECRET,
-};
+})
